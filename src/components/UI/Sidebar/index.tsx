633961dc3d98e38430533c4d0eec1396731a5fef
reshad21 <miakala7@gmail.com>
2024-10-09 16:23:32 +0600

    rename nextconfig file

--- src/components/UI/Sidebar/index.tsx
+++ src/components/UI/Sidebar/index.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { useUser } from "@/context/user.provider";
 import { Button } from "@nextui-org/button";
+import Image from "next/image";
 import Link from "next/link";
+
+import { useUser } from "@/context/user.provider";
 import { SidebarOptions } from "./SidebarOptions";
 import { adminLinks, userLinks } from "./constants";
 
@@ -12,11 +14,17 @@ const Sidebar = () => {
     <div>
       <div className="rounded-xl bg-default-100 p-2">
         <div className="h-[330px] w-full rounded-md">
-          <h1>Ekhane user er profile picture hobe</h1>
+          <Image
+            alt="profile"
+            className="w-full h-full object-cover rounded-md"
+            height={330}
+            src={user?.profilePhoto as string}
+            width={330}
+          />
         </div>
         <div className="my-3">
-          <h1 className="text-2xl font-semibold">Mofiz</h1>
-          <p className="break-words text-sm">mofiz@gmail.com</p>
+          <h1 className="text-2xl font-semibold">{user?.name}</h1>
+          <p className="break-words text-sm">{user?.email}</p>
         </div>
         <Button
           as={Link}
